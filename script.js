0bb4b2155ec23c1c1e965de946cec6c977582b56
Ashish Choudhary <119871661+aashish-jaat12@users.noreply.github.com>
2023-08-10 10:59:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+ 
+//  function play (audioplay) {
+//     let audio = new Audio (audioplay);
+//     audio.loop=true;
+//     audio.play();
+    
+// }
+
+
+// play("audio1.WAV")
+
+// const audio =document.getElementById('autoplay')
+
+// function autoplay() {
+	
+// 	audio.Play();
+// }
+// autoplay()
